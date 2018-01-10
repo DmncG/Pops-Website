4f768b794f710a46c70e90c8e4ece52b45bf551e
DmncG <drbgarcia389@yahoo.com>
2018-01-10 19:07:55 +0800

    feat navbar effects

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,31 @@
 const $window = $(window);
-$window.on('scroll', () => {
+const $navWrapper = $(".nav-wrapper")
+$window.on("scroll", () => {
     const pos = $window.scrollTop();
     console.log("windowpos", pos)
+    if(pos < 10 && $(".active")){
+        $navWrapper.removeClass("active")
+    }
+    else if(pos > 10){
+        $navWrapper.addClass("active")
+    }
 })
 
+$navWrapper.mouseover(() => {
+    console.log('mouseentered***')
+    $navWrapper.removeClass("active")
+})
  /* two things I should be able to do: 
  1. When I click on the navbar, it goes directly to the section of the page. **check**
  2. When I am scrolling, the navbar disappears, but when I hover back to the navbar, it reappears 
+
+ Approach:
+
+ on scroll add active
+ on hover disable active
  */
+
+ //scrollTop navbar button functions
 
 function navScrollHome () {
  $window.scrollTop(0)
@@ -22,6 +40,8 @@ function navScrollProducts () {
 function navScrollContact () {
     $window.scrollTop($(document).height())
 }
+
+//navbar scroll effects
 
 
 
