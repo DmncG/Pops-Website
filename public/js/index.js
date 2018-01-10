3582fb2fefef0c73d8d5e375547be2568e5cc78f
DmncG <drbgarcia389@yahoo.com>
2018-01-10 17:41:56 +0800

    feat scroll to different sections onclick

--- public/js/index.js
+++ public/js/index.js
@@ -5,16 +5,23 @@ $window.on('scroll', () => {
 })
 
  /* two things I should be able to do: 
- 1. When I click on the navbar, it goes directly to the section of the page.
- 2. When I am scrolling, the navbar disappears, but when I hover back to the navbar, it reappears
+ 1. When I click on the navbar, it goes directly to the section of the page. **check**
+ 2. When I am scrolling, the navbar disappears, but when I hover back to the navbar, it reappears 
  */
 
 function navScrollHome () {
-
  $window.scrollTop(0)
-
  }
 
+function navScrollProducts () {
+    const $products = $(products)
+    const productOffset = $products.offset().top
+    $window.scrollTop(productOffset)    
+}
+
+function navScrollContact () {
+    $window.scrollTop($(document).height())
+}
 
 
 
