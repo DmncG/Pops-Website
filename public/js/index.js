432ea04fb0a6bf96aebd5c4c85fe0faf24d79ebc
DmncG <drbgarcia389@yahoo.com>
2018-01-10 15:37:03 +0800

    initiating jquery on navbar

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+const $window = $(window);
+$window.on('scroll', () => {
+    const pos = $window.scrollTop();
+    console.log("windowpos", pos)
+})
+
+ /* two things I should be able to do: 
+ 1. When I click on the navbar, it goes directly to the section of the page.
+ 2. When I am scrolling, the navbar disappears, but when I hover back to the navbar, it reappears
+ */
+
+ 
