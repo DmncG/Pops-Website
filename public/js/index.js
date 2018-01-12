745f409e134e03aec8db708ebb5a89d7b49c2c27
DmncG <drbgarcia389@yahoo.com>
2018-01-12 15:04:14 -0500

    feat navbar effects working

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,20 @@
 const $window = $(window);
 const $navWrapper = $(".nav-wrapper")
+
 $window.on("scroll", () => {
     const pos = $window.scrollTop();
-    console.log("windowpos", pos)
     if(pos < 10 && $(".active")){
         $navWrapper.removeClass("active")
     }
-    else if(pos > 10){
+    else{
         $navWrapper.addClass("active")
     }
 })
 
-$navWrapper.mouseover(() => {
-    console.log('mouseentered***')
+$navWrapper.mouseover(function () {
     $navWrapper.removeClass("active")
+    
 })
- /* two things I should be able to do: 
- 1. When I click on the navbar, it goes directly to the section of the page. **check**
- 2. When I am scrolling, the navbar disappears, but when I hover back to the navbar, it reappears 
-
- Approach:
-
- on scroll add active
- on hover disable active
- */
 
  //scrollTop navbar button functions
 
